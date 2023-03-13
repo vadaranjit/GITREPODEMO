46d704f87d912863698404d2a732e01c62a9acc6
Ranjit Vadar <121163141+vadaranjit@users.noreply.github.com>
2023-03-14 00:09:05 +0530

    for pull

--- DalalStreet17SeptFrameworkGITBASH/src/test/java/com/ds/testLayer/GitBashDemo.java
+++ DalalStreet17SeptFrameworkGITBASH/src/test/java/com/ds/testLayer/GitBashDemo.java
@@ -5,6 +5,8 @@ public class GitBashDemo {
 	public static void main(String[] args) 
 	{
 		System.out.print("Hello world- from local reppo");
+		System.out.print("Hello world- from github reppo");
+		
 	}
 
 }
